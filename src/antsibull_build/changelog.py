ansible-community/antsibull

use tagged releases to pull core porting guides

src/antsibull/changelog.py
@@ -33,6 +33,7 @@ from packaging.version import Version as PypiVer
 from semantic_version import Version as SemVer
 
 from antsibull.collection_meta import CollectionsMetadata
+from antsibull.utils.urls import get_documentation_repo_raw_url
 
 
 class ChangelogData:
@@ -155,12 +156,14 @@ def read_changelog_file(tarball_path: str, is_ansible_core=False) -> bytes | Non
     return read_file(tarball_path, matcher)
 
 
-def get_porting_guide_filename(version: PypiVer):
-    if version.major == 2 and version.minor == 10:
-        basename = "porting_guide_base"
-    else:
-        basename = "porting_guide_core"
-    return f"docs/docsite/rst/porting_guides/{basename}_{version.major}.{version.minor}.rst"
+def get_core_porting_guide_url(version: PypiVer):
+    major_minor = f"{version.major}.{version.minor}"
+    return (
+        get_documentation_repo_raw_url(version)
+        + f"/v{version}"
+        + "/docs/docsite/rst/porting_guides"
+        + f"/porting_guide_core_{major_minor}.rst"
+    )
 
 
 class CollectionChangelogCollector:
@@ -320,11 +323,7 @@ class AnsibleCoreChangelogCollector:
         self.changelog = ChangelogData.concatenate(changelogs)
 
     async def download_porting_guide(self, aio_session: aiohttp.client.ClientSession):
-        branch_url = (
-            "https://raw.githubusercontent.com/ansible/ansible-documentation/devel"
-        )
-
-        query_url = f"{branch_url}/{get_porting_guide_filename(self.latest)}"
+        query_url = get_core_porting_guide_url(self.latest)
         async with aio_session.get(query_url) as response:
             self.porting_guide = await response.read()
 

tests/test_changelog.py
@@ -1,0 +1,41 @@
+# Copyright (C) 2023 Maxwell G <maxwell@gtmx.me>
+# SPDX-License-Identifier: GPL-3.0-or-later
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or
+# https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# It doesn't make sense to split up the long URLs
+# flake8: noqa E501
+
+import pytest
+from packaging.version import Version as PypiVer
+
+from antsibull.changelog import get_core_porting_guide_url
+
+
+@pytest.mark.parametrize(
+    "version, expected",
+    [
+        pytest.param(
+            PypiVer("2.12.0"),
+            "https://github.com/ansible/ansible/raw/v2.12.0/docs/docsite/rst/porting_guides/porting_guide_core_2.12.rst",
+        ),
+        pytest.param(
+            PypiVer("2.13.0"),
+            "https://github.com/ansible/ansible/raw/v2.13.0/docs/docsite/rst/porting_guides/porting_guide_core_2.13.rst",
+        ),
+        pytest.param(
+            PypiVer("2.13.11"),
+            "https://github.com/ansible/ansible-documentation/raw/v2.13.11/docs/docsite/rst/porting_guides/porting_guide_core_2.13.rst",
+        ),
+        pytest.param(
+            PypiVer("2.14.8"),
+            "https://github.com/ansible/ansible-documentation/raw/v2.14.8/docs/docsite/rst/porting_guides/porting_guide_core_2.14.rst",
+        ),
+        pytest.param(
+            PypiVer("2.16.0a1"),
+            "https://github.com/ansible/ansible-documentation/raw/v2.16.0a1/docs/docsite/rst/porting_guides/porting_guide_core_2.16.rst",
+        ),
+    ],
+)
+def test_get_core_porting_guide_url(version: PypiVer, expected: str):
+    assert get_core_porting_guide_url(version) == expected

ansible-community/antsibull-build

src/antsibull_build/changelog.py
@@ -53,6 +53,8 @@ from antsibull_fileutils.yaml import load_yaml_bytes
 from packaging.version import Version as PypiVer
 from semantic_version import Version as SemVer
 
+from antsibull_build.utils.urls import get_documentation_repo_raw_url
+
 mlog = log.fields(mod=__name__)
 
 
@@ -247,8 +249,14 @@ def read_changelog_file(tarball_path: str, is_ansible_core=False) -> bytes | Non
     return read_file(tarball_path, matcher)
 
 
-def get_porting_guide_filename(version: PypiVer):
-    return f"docs/docsite/rst/porting_guides/porting_guide_core_{version.major}.{version.minor}.rst"
+def get_core_porting_guide_url(version: PypiVer):
+    major_minor = f"{version.major}.{version.minor}"
+    return (
+        get_documentation_repo_raw_url(version)
+        + f"/v{version}"
+        + "/docs/docsite/rst/porting_guides"
+        + f"/porting_guide_core_{major_minor}.rst"
+    )
 
 
 class CollectionChangelogCollector:
@@ -409,11 +417,7 @@ class AnsibleCoreChangelogCollector:
         self.changelog = ChangelogData.concatenate(changelogs)
 
     async def download_porting_guide(self, aio_session: aiohttp.client.ClientSession):
-        branch_url = (
-            "https://raw.githubusercontent.com/ansible/ansible-documentation/devel"
-        )
-
-        query_url = f"{branch_url}/{get_porting_guide_filename(self.latest)}"
+        query_url = get_core_porting_guide_url(self.latest)
         async with aio_session.get(query_url) as response:
             self.porting_guide = await response.read()
 

tests/test_changelog.py
@@ -1,0 +1,41 @@
+# Copyright (C) 2023 Maxwell G <maxwell@gtmx.me>
+# SPDX-License-Identifier: GPL-3.0-or-later
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or
+# https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# It doesn't make sense to split up the long URLs
+# flake8: noqa E501
+
+import pytest
+from packaging.version import Version as PypiVer
+
+from antsibull_build.changelog import get_core_porting_guide_url
+
+
+@pytest.mark.parametrize(
+    "version, expected",
+    [
+        pytest.param(
+            PypiVer("2.12.0"),
+            "https://github.com/ansible/ansible/raw/v2.12.0/docs/docsite/rst/porting_guides/porting_guide_core_2.12.rst",
+        ),
+        pytest.param(
+            PypiVer("2.13.0"),
+            "https://github.com/ansible/ansible/raw/v2.13.0/docs/docsite/rst/porting_guides/porting_guide_core_2.13.rst",
+        ),
+        pytest.param(
+            PypiVer("2.13.11"),
+            "https://github.com/ansible/ansible-documentation/raw/v2.13.11/docs/docsite/rst/porting_guides/porting_guide_core_2.13.rst",
+        ),
+        pytest.param(
+            PypiVer("2.14.8"),
+            "https://github.com/ansible/ansible-documentation/raw/v2.14.8/docs/docsite/rst/porting_guides/porting_guide_core_2.14.rst",
+        ),
+        pytest.param(
+            PypiVer("2.16.0a1"),
+            "https://github.com/ansible/ansible-documentation/raw/v2.16.0a1/docs/docsite/rst/porting_guides/porting_guide_core_2.16.rst",
+        ),
+    ],
+)
+def test_get_core_porting_guide_url(version: PypiVer, expected: str):
+    assert get_core_porting_guide_url(version) == expected
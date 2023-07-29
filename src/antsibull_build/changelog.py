e102d7483d93bca73aa22c4b217870aaa5799c75
Maxwell G <maxwell@gtmx.me>
2023-07-29 01:35:00 +0000

    use tagged releases to pull core porting guides

--- src/antsibull_build/changelog.py
+++ src/antsibull_build/changelog.py
@@ -53,6 +53,8 @@ from antsibull_fileutils.yaml import load_yaml_bytes
 from packaging.version import Version as PypiVer
 from semantic_version import Version as SemVer
 
+from antsibull_build.utils.urls import get_documentation_repo_raw_url
+
 mlog = log.fields(mod=__name__)
 
 
@@ -247,8 +249,14 @@ def read_changelog_file(tarball_path: str, is_ansible_core=False) -> bytes | Non
     return read_file(tarball_path, matcher)
 
 
-def get_porting_guide_filename(version: PypiVer):
-    return f"docs/docsite/rst/porting_guides/porting_guide_core_{version.major}.{version.minor}.rst"
+def get_core_porting_guide_url(version: PypiVer):
+    major_minor = f"{version.major}.{version.minor}"
+    return (
+        get_documentation_repo_raw_url(version)
+        + f"/v{version}"
+        + "/docs/docsite/rst/porting_guides"
+        + f"/porting_guide_core_{major_minor}.rst"
+    )
 
 
 class CollectionChangelogCollector:
@@ -409,11 +417,7 @@ class AnsibleCoreChangelogCollector:
         self.changelog = ChangelogData.concatenate(changelogs)
 
     async def download_porting_guide(self, aio_session: aiohttp.client.ClientSession):
-        branch_url = (
-            "https://raw.githubusercontent.com/ansible/ansible-documentation/devel"
-        )
-
-        query_url = f"{branch_url}/{get_porting_guide_filename(self.latest)}"
+        query_url = get_core_porting_guide_url(self.latest)
         async with aio_session.get(query_url) as response:
             self.porting_guide = await response.read()
 
